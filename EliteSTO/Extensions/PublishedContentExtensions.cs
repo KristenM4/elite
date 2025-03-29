15b1f16d5b2607556b0c3fb6784943a335707082
Kristen <kristen4810@gmail.com>
2025-03-29 20:35:11 +0000

    get metadata for pages

--- EliteSTO/Extensions/PublishedContentExtensions.cs
+++ EliteSTO/Extensions/PublishedContentExtensions.cs
@@ -1,20 +1,37 @@
 ﻿using Umbraco.Cms.Web.Common.PublishedModels;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
-namespace EliteSTO.Extensions
-{
-    public static class PublishedContentExtensions
-    {
-        public static HomePage? GetHomePage(this IPublishedContent publishedContent)
-        {
-            return publishedContent.AncestorOrSelf<HomePage>();
-        }
+namespace EliteSTO.Extensions;
 
-        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent)
-        {
-            var homePage = GetHomePage(publishedContent);
-            if (homePage is null) return null;
-            return homePage.FirstChild<SiteSettings>();
-        }
+public static class PublishedContentExtensions
+{
+    public static HomePage? GetHomePage(this IPublishedContent publishedContent)
+    {
+        return publishedContent.AncestorOrSelf<HomePage>();
+    }
+
+    public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent)
+    {
+        var homePage = GetHomePage(publishedContent);
+        if (homePage is null) return null;
+        return homePage.FirstChild<SiteSettings>();
+    }
+
+    public static string GetMetaTitleOrName(this IPublishedContent publishedContent, string? metaTitle)
+    {
+        if (!string.IsNullOrWhiteSpace(metaTitle)) return metaTitle;
+
+        return publishedContent.Name;
+    }
+
+    public static string? GetSiteName(this IPublishedContent publishedContent)
+    {
+        var homePage = publishedContent.GetHomePage();
+        if (homePage == null) return null;
+
+        var siteSettings = homePage.GetSiteSettings();
+        if (siteSettings is null) return null;
+
+        return siteSettings?.SiteName ?? null;
     }
 }
